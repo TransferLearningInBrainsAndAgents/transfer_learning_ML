47f880bd08a25b56e9983e8d9a2eef60ffb4519c
georgedimitriadis <gdimitri@hotmail.com>
2023-09-28 17:31:33 +0100

    Added some forgotten scripts

---
+++ Environments/Unity/Target_Trap_Manipulandum/Assets/Scripts/Logic/CameraObservationsController.cs
@@ -0,0 +1,80 @@
+
+
+using System;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+
+public class CameraObservationsController : MonoBehaviour
+{
+    private GraphicsFormat format;
+    public Camera observationCamera;
+
+
+    // Event onNewScreenResolution that triggers this is thrown by the RatController
+    void RecalculateScreenRes(int width, int height)
+    {
+        Screen.SetResolution(width, height, FullScreenMode.Windowed);
+    }
+    
+    private void Start()
+    {
+        EventManager.Instance.onNewScreenResolution.AddListener(RecalculateScreenRes);
+
+        EventManager.Instance.onNeedingNewPixelsObservation.AddListener(PrepareNewObservation);
+    }
+
+
+    /// <summary>
+    /// <c>PrepareNewObservation</c> is called when the environment has finished updating itself and can now generate the new camera observation to pass to the agent if required.
+    /// </summary>
+    private void PrepareNewObservation()
+    {
+        observationCamera.Render();
+        CaptureScreenShot();
+    }
+
+    // The following two functions will render the camera and pass the generated pixels to a RenderTexture. That will be encoded into an PNG, and those bytes will be sent
+    // to the ReqRepClient to keep in memory until the agents asks for a camera observation
+
+    public void CaptureScreenShot()
+    {
+        var rt = default(RenderTexture);
+
+        while (rt == default(RenderTexture))
+            rt = RenderTexture.GetTemporary(Screen.width, Screen.height, 0);
+
+        format = rt.graphicsFormat;
+
+        ScreenCapture.CaptureScreenshotIntoRenderTexture(rt);
+
+        AsyncGPUReadback.Request(rt, 0, TextureFormat.RGBA32, OnCompleteReadback);
+
+        RenderTexture.ReleaseTemporary(rt);
+    }
+
+
+    void OnCompleteReadback(AsyncGPUReadbackRequest request)
+    {
+        if (request.hasError)
+        {
+            Debug.Log("GPU readback error detected.");
+            return;
+        }
+
+        try
+        {
+            byte[]  array = request.GetData<byte>().ToArray();
+
+            byte[] pngBytes = ImageConversion.EncodeArrayToPNG(array, format, (uint)Screen.width, (uint)Screen.height);
+            
+            EventManager.Instance.onPixelsObservationReady.Invoke(pngBytes);
+             
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+
+}
---
+++ Environments/Unity/Target_Trap_Manipulandum/Assets/Scripts/Logic/RewardController.cs
@@ -0,0 +1,67 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardController : MonoBehaviour
+{
+
+    private float action_reward = 0;
+    private float area_reward = 0;
+    private bool port_reward = false;
+        
+    void Start()
+    {
+        EventManager.Instance.onRewardFromAction.AddListener(SaveNewRewardDueToAction);
+        EventManager.Instance.onNeedingNewTotalReward.AddListener(GenerateNewTotalReward);
+    }
+
+    private void GenerateNewTotalReward()
+    {
+        float totalReward = action_reward + area_reward + (port_reward ? RewardStructure.Instance.RewPortPokedCorrectly: 0f);
+      
+        EventManager.Instance.onRewardReady.Invoke(totalReward);
+        action_reward = 0f;
+        area_reward = 0f;
+        port_reward = false;
+           
+    }
+
+    private void SaveNewRewardDueToAction(float new_reward)
+    {
+        action_reward = new_reward;
+    }
+
+    // This is called from the Visual Scripting Graph in the VRGame Object,
+    // by the Transition (Success State -> Running_Trial State), 
+    // if there is a Reward Port touched (OnEnter) event.
+    private void SaveNewRewardDueToPortTouched()
+    {
+        EventManager.Instance.onStopFeaturesSending.Invoke();
+        port_reward = true;
+        EventManager.Instance.onRedoFeaturesObservations.Invoke(2);
+    }
+
+
+    //This is called from the Visual Scripting Graph in the VRGame Object,
+    // when the reward areas are crossed for the first time after n seconds
+    public void SaveNewRewardDueToAreaTouched(string area_type)
+    {
+        EventManager.Instance.onStopFeaturesSending.Invoke();
+        switch (area_type)
+        {
+            case string value when value.Contains("High"):
+                area_reward = RewardStructure.Instance.AreaHighInterest;
+                break;
+            case string value when value.Contains("Med"):
+                area_reward = RewardStructure.Instance.AreaMedInterest;
+                break;
+            case string value when value.Contains("Low"):
+                area_reward = RewardStructure.Instance.AreaLowInterest;
+                break;
+        }
+        EventManager.Instance.onRedoFeaturesObservations.Invoke(1);
+    }
+
+    
+}
